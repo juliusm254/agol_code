9e8229dc23bf00b53ed97c85c2c830078eb1779f
bassbassbuff <bassbassbuff@gmail.com>
2022-08-27 18:16:29 -0400

    Before advanced serializers

--- agol/operations/selectors.py
+++ agol/operations/selectors.py
@@ -1,4 +1,6 @@
 from django.db.models.query import QuerySet
+from django.db.models import Prefetch, Count, Q
+from operations.models import SafetyChecklist
 from customers.models import Order
 from django.shortcuts import get_object_or_404
 
@@ -7,6 +9,11 @@ def order_list(order_status) -> QuerySet[Order]:
 
 
 
-def checklist_details(pk) -> QuerySet[Order]:
-    return(get_object_or_404(Order, id=pk))
+def checklist_details(pk) -> QuerySet[SafetyChecklist]:
+    return(SafetyChecklist.objects.select_related('order').filter(order_id=pk))
+
+    return(Order.objects.get(id=pk).prefetch_related(
+            Prefetch('order_id',
+            queryset=SafetyChecklist.objects.all())
+        ))
 
---
+++ agol/operations/utils.py
@@ -0,0 +1,7 @@
+# def inline_serializer(*, fields, data=None, **kwargs):
+#     serializer_class = create_serializer_class(name='', fields=fields)
+
+#     if data is not None:
+#         return serializer_class(data=data, **kwargs)
+
+#     return serializer_class(**kwargs)
